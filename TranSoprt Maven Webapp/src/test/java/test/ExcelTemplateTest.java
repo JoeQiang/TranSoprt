52796f5c69f9e9f3d1da5b95991b9050796f30d1
Joe <jm_hxq@163.com>
2016-01-16 19:17:27 +0800

    新增Excel模版、模版工厂

---
+++ TranSoprt Maven Webapp/src/test/java/test/ExcelTemplateTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.dom4j.DocumentException;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.vanroid.transopt.uitls.ExcelTemplate;
+import com.vanroid.transopt.uitls.ExcelTemplateFactory;
+
+public class ExcelTemplateTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() throws DocumentException {
+
+		ExcelTemplateFactory factory = ExcelTemplateFactory.getInstance();
+		ExcelTemplate template = factory.getTemplate("supplier");
+		// template.createExcelTemplate();
+		
+	}
+
+}
